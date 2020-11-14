dc1744565998611336c8976c9f4b46bd62b87e86
Pius-emy <47452183+Pius-emy@users.noreply.github.com>
2020-11-14 11:53:33 +0100

    Add files via upload

---
+++ Volatility Indicator/Volatility Indicator/Volatility Indicator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using cAlgo.API;
+using cAlgo.API.Internals;
+using cAlgo.API.Indicators;
+using cAlgo.Indicators;
+
+namespace cAlgo
+{
+    [Indicator(IsOverlay = false, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class VolatilityIndicator : Indicator
+    {
+        [Parameter("Period", DefaultValue = 10)]
+        public int Period { get; set; }
+
+        [Output("Volatility", LineColor = "Green", IsHistogram = false, LineStyle = LineStyle.Solid, PlotType = PlotType.Line, Thickness = 2)]
+        public IndicatorDataSeries Volatility { get; set; }
+
+        [Output("MidLine", LineColor = "Gray", LineStyle = LineStyle.Solid, PlotType = PlotType.Line, Thickness = 2)]
+        public IndicatorDataSeries MidLine { get; set; }
+
+        private double timeInSeconds;
+
+        protected override void Initialize()
+        {
+        }
+
+        public override void Calculate(int index)
+        {
+            int lastIndex = (index - Period + 1) < 0 ? 0 : (index - Period + 1);
+            var volatility = 0.0;
+            for (int i = index; i >= lastIndex; i--)
+            {
+                var time = Bars.LastBar.OpenTime == Bars[i].OpenTime ? (this.Server.Time - Bars.LastBar.OpenTime).TotalSeconds : (Bars[i + 1].OpenTime - Bars[i].OpenTime).TotalSeconds;
+
+                volatility += Math.Abs(this.Bars[i].Close - this.Bars[i].Open) / time;
+            }
+            this.Volatility[index] = volatility / 0.0001;
+            this.MidLine[index] = 0.1;
+        }
+    }
+}
